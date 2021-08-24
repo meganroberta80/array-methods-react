d2316dec459b99d3309230333fbb6fd784ff174f
meganroberta80 <meganroberta89@gmail.com>
2021-08-24 15:26:36 -0700

    Solved questions 1-5.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import planetData from './planetData';
 import './App.css';
 
 class App extends React.Component {
-  // 1. Given the array of planet data render a list of each of the
-  // planet names.
-  renderPlanets() {
-    return planetData.map((planet) => {
-      return (
-        <p>{planet.name}</p>
-      );
-    });
-  };
-
-
-  // 2. Given the array of planet data, map through the array to
-  // render a list displaying each of the plenet's name and their
-  // diameter.
+  // 1. Given the array of planet data render a list of each of the planet names.
   // renderPlanets() {
-  //   // Place code here
+  //   return planetData.map((planet) => {
+  //     return (
+  //       <p>{planet.name}</p>
+  //     );
+  //   });
+  // };
+
+
+  // 2. Given the array of planet data, map through the array to render a list displaying each of the plnet's name and their diameter.
+  // renderPlanets() {
+  //   return planetData.map((planet) => {
+  //     return (
+  //       <div>
+  //         <h2>{planet.name}</h2>
+  //         <p>Diameter: {planet.diameter}</p>
+  //       </div>
+  //     )
+  //   })
   // }
 
 
@@ -26,7 +30,14 @@ class App extends React.Component {
   // render a list display just the name and length of day of
   // each planet.
   // renderPlanets() {
-  //   // Place code here
+  //   return planetData.map((planet) => {
+  //     return (
+  //       <div>
+  //         <h2>{planet.name}</h2>
+  //         <p>Length of Day: {planet.lengthOfDay}</p>
+  //       </div>
+  //     )
+  //   })
   // }
 
 
@@ -40,16 +51,33 @@ class App extends React.Component {
   // </div>
 
   // renderPlanets() {
-  //   // Place code here
+  //   return planetData.map((planet, idx) => {
+  //     return (
+  //       <div key={idx}>
+  //         <h2>{idx}. {planet.name}</h2>
+  //         <p>Length of Day: {planet.lengthOfDay}</p>
+  //       </div>
+  //     )
+  //   })
   // }
 
 
-  // 5. Given the array of planet data 'find' the planet with the name
-  // of 'Earth' and render it's name, diameter, and length of day. Use
-  // one of the array iterator methods to find the planet.
-  // renderPlanets() {
-  //   // Place code here
-  // }
+  // 5. Given the array of planet data 'find' the planet with the name of 'Earth' and render its name, diameter, and length of day. Use one of the array iterator methods to find the planet.
+  renderPlanets() {
+    const earth = planetData.find((planet) => {
+      return planet.name === 'Earth'
+    })
+
+    const earthJSX = (
+      <div>
+        <h2>{earth.name}</h2>
+        <p>Diameter: {earth.diameter}</p>
+        <p>Length of Day: {earth.lengthOfDay}</p>
+      </div>
+    )
+
+    return earthJSX
+  }
 
 
   // 6. Given the array of planet data 'find' the planet with the length
@@ -84,7 +112,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Planets Data</h1>
-        {this.renderPlanets()}  
+        {this.renderPlanets()}
       </div>
     );
   }
